b3c2999733653821751d59c072691399bfaa7b87
Kyle Pfromer <kpfromer2@gmail.com>
2021-01-19 12:27:15 -0700

    chore: update test app usage

--- packages/test-app/pages/index.tsx
+++ packages/test-app/pages/index.tsx
@@ -1,9 +1,11 @@
 import Img from 'react-optimized-image';
 import Image from '../images/test.jpg';
+import ImageTwo from '../images/test-2.jpg';
+import ImageThree from '../images/test-3.jpg';
 
 export interface HomeProps {}
 
-const Home: React.FC<HomeProps> = () => {
+const FirstImg = () => {
   const width = 5472;
   const height = 3648;
   const aspectRatio = width / height;
@@ -11,15 +13,57 @@ const Home: React.FC<HomeProps> = () => {
   const wantedWidth = 750;
 
   return (
-    <>
-      {/* <Img /> */}
+    <Img
+      src={Image}
+      placeholder="trace"
+      width={wantedWidth}
+      height={Math.round(wantedWidth / aspectRatio)}
+    />
+  );
+};
 
-      <Img
-        src={Image}
-        placeholder
-        width={wantedWidth}
-        height={Math.round(wantedWidth / aspectRatio)}
-      />
+const SecondImg = () => {
+  const width = 3667;
+  const height = 5500;
+  const aspectRatio = width / height;
+
+  const wantedWidth = 500;
+
+  return (
+    <Img
+      src={ImageTwo}
+      placeholder="lqip"
+      width={wantedWidth}
+      height={Math.round(wantedWidth / aspectRatio)}
+    />
+  );
+};
+
+const ThirdImg = () => {
+  const width = 6000;
+  const height = 4000;
+  const aspectRatio = width / height;
+
+  const wantedWidth = 750;
+
+  return (
+    <Img
+      src={ImageThree}
+      webp
+      placeholder="lqip"
+      width={wantedWidth}
+      height={Math.round(wantedWidth / aspectRatio)}
+    />
+  );
+};
+
+const Home: React.FC<HomeProps> = () => {
+  return (
+    <>
+      <FirstImg />
+      <ThirdImg />
+      <div style={{ paddingTop: '500em' }} />
+      <SecondImg />
     </>
   );
 };
